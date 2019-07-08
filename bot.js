23f280d87bbccb55454be863540a10d097720eb6
Frank Sluyter <47513385+Slayerfroggie@users.noreply.github.com>
2019-07-08 14:43:33 +1000

    Update bot.js
    minor fixes to undefined help commands and added console logging for commands and which server the command was called on.

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ function processCommand(receivedMessage) {
     let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
     let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
     let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
-    console.log("full command: " + fullCommand)
+    console.log("Full command: " + fullCommand)
     console.log("Command received: " + primaryCommand)
     console.log("Arguments: " + arguments) // There may not be any arguments
     if (primaryCommand == "frhelp") {
@@ -45,12 +45,13 @@ function processCommand(receivedMessage) {
 }
 
 function helpCommand(arguments, receivedMessage, fullCommand) {
-    //if(arguments[1] == ""){
-    //receivedMessage.channel.send("I'm not sure what you need help with. Try `!frhelp [topic/command]`")
+    console.log("Help command run on the server: " + receivedMessage.guild.name)
     if (arguments[0] == "multiply") {
         receivedMessage.channel.send("to multiply 2 or more numbers together, try typing [!frmultiply # #]")
     } else if (arguments[0] == "roll") {
         receivedMessage.channel.send("to roll a dice, try typing [!frroll #d#] `e.g. 1d4 or 3d12`")
+    } else if (arguments[0] == undefined) {
+        receivedMessage.channel.send("Not giving me anything to work with is really not helpful.")
     } else {
         let badCommand = fullCommand.split("frhelp ")
         //console.log("Bad help command: " + badCommand[1])
@@ -59,6 +60,7 @@ function helpCommand(arguments, receivedMessage, fullCommand) {
 }
 
 function multiplyCommand(arguments, receivedMessage) {
+    console.log("Multiply command run on the server: " + receivedMessage.guild.name)
     if (arguments.length < 2) {
         receivedMessage.channel.send("Not enough values to multiply. Try `!frmultiply 2 4 10` or `!frmultiply 5.2 7`")
         return
@@ -71,6 +73,7 @@ function multiplyCommand(arguments, receivedMessage) {
 }
 
 function diceRollCommand(arguments, receivedMessage) {
+    console.log("Dice roll command run on the server: " + receivedMessage.guild.name)
     if (arguments.length != 1) {
         receivedMessage.channel.send("Incorrect dice roll input. For correct input please type the quantity of dice being rolled, followed by a 'd' and the dice. `e.g. 1d4 or 3d12`")
         return
@@ -109,8 +112,10 @@ function processImDad(dadArguments, receivedMessage, dadCommand){
     {
         if (dadArguments[i] == "i'm" || dadArguments[i] == "im" || dadArguments[i] == "I'm" ||dadArguments[i] == "Im" ) { //detecting the keyword
             if (dadArguments[i + 1] == "dad" || dadArguments[i + 1] == "Dad") {
+                console.log("Dad comeback command run on the server: " + receivedMessage.guild.name)
                 receivedMessage.channel.send("No you're not, you're " + receivedMessage.author.username + ".")
             } else {
+                console.log("Dad comeback command run on the server: " + receivedMessage.guild.name)
                 dadString = dadCommand.split(dadArguments[i])//spliting the same string in the start of the loop, but spliting by the keyword
                 dadComeback = dadString[1] //putting the string after the keyword into the 
                 receivedMessage.channel.send("Hi" + dadComeback + ", I'm Dad!")
@@ -123,8 +128,9 @@ function processImDad(dadArguments, receivedMessage, dadCommand){
 function processDadKYS(dadArguments, receivedMessage) {
     for (i = 0; i <= dadArguments.length; i++){
         if (dadArguments[i] == "KYS" || dadArguments[i] == "kys"){
+            console.log("Dad kys command run on the server: " + receivedMessage.guild.name)
             receivedMessage.channel.send("Alright "+ receivedMessage.author.username + ", that was very rude. Instead, take your own advice.")
+            break
         }
-        break
     }
 }
